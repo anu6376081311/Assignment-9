6e084bfd70b34e341126ed527557555448df5069
anumeena <111556950+anu6376081311@users.noreply.github.com>
2023-03-14 23:03:58 +0530

    Add files via upload

---
+++ A9(1).c
@@ -0,0 +1,47 @@
+#include<stdio.h>
+int main()
+{
+ int n;
+ printf(" Enter a number :");
+ scanf("%d",&n);
+ switch(n)
+ {
+    case 1 :
+    printf("january and 31 days ");
+    break;
+    case 2 :
+    printf("february and 28 or 29 days");
+    break;
+    case 3 :
+    printf("march and 31 days ");
+    break;
+    case 4 :
+    printf("april and 30 days ");
+    break;
+    case 5 :
+    printf("may and 31 days ");
+    break;
+    case 6 :
+    printf("june and 30 days ");
+    break;
+    case 7 :
+    printf("july and 31 days ");
+    break;
+    case 8 :
+    printf("august and 31 days ");
+    break;
+    case 9 :
+    printf("september and 30 days ");
+    break;
+    case 10 :
+    printf("october and 31 days ");
+    break;
+    case 11 :
+    printf("november and 30 days ");
+    break;
+    case 12 :
+    printf("december and 31 days ");
+    break;
+ }
+ return 0;
+}
